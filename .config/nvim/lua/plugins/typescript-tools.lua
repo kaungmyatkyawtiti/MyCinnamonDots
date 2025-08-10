b0f327c830011f9009eeaaa7253134d10e951162
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-08-10 22:55:06 +0630

    My tools config files

--- .config/nvim/lua/plugins/typescript-tools.lua
+++ .config/nvim/lua/plugins/typescript-tools.lua
@@ -1,5 +1,12 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  -- opts = {},
+  opts = {
+    settings = {
+      tsserver_file_preferences = {
+        quotePreference = "single",
+      },
+    },
+  },
 }
