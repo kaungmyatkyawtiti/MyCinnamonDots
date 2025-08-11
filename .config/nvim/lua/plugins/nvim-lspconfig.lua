7637643b0c4a4b89fc4f4f85ff309665a526e3cf
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-08-11 20:58:43 +0630

    My tools config files

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,27 @@
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       -- autoformat = false,
+--       inlay_hints = { enabled = false },
+--     },
+--   },
+-- }
+
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      -- autoformat = false,
-      inlay_hints = { enabled = false },
-    },
+    opts = function(_, opts)
+      opts.inlay_hints = { enabled = false }
+      opts.servers = opts.servers or {}
+      opts.setup = opts.setup or {}
+
+      opts.setup.taplo = function(_, _)
+        local fname = vim.fn.expand("%:t")
+        if fname == "starship.toml" then
+          return true -- returning true skips default setup (disables Taplo here)
+        end
+      end
+    end,
   },
 }
