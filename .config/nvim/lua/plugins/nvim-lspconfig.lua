d90bcd8c199276763658b5e3082dc2d5ab15b32b
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-08-26 01:42:52 +0630

    chore(neovim): update nvim-lspconfig configuration

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,21 +1,3 @@
--- return {
---   {
---     "neovim/nvim-lspconfig",
---     opts = function(_, opts)
---       opts.inlay_hints = { enabled = false }
---       opts.servers = opts.servers or {}
---       opts.setup = opts.setup or {}
---
---       opts.setup.taplo = function(_, _)
---         local fname = vim.fn.expand("%:t")
---         if fname == "starship.toml" then
---           return true -- returning true skips default setup (disables Taplo here)
---         end
---       end
---     end,
---   },
--- }
-
 return {
   {
     "neovim/nvim-lspconfig",
@@ -37,17 +19,21 @@ return {
       opts.setup.eslint = function(_, _)
         require("lspconfig").eslint.setup({
           settings = {
-            format = { enable = true }, -- optional
+            format = { enable = true },
+            workingDirectories = { mode = "auto" }, -- important for monorepos
+            experimental = { useFlatConfig = true }, -- flat config support
           },
           on_attach = function(client, bufnr)
             -- Disable automatic formatting on change
             client.server_capabilities.documentFormattingProvider = false
 
-            -- Run ESLint fix on save
+            -- Async ESLint fix on save to avoid freeze
             vim.api.nvim_create_autocmd("BufWritePre", {
               buffer = bufnr,
               callback = function()
-                vim.cmd("EslintFixAll")
+                vim.defer_fn(function()
+                  vim.cmd("EslintFixAll")
+                end, 0)
               end,
             })
           end,
