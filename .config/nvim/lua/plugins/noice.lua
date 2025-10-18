2b147a82ce3332c5984ce0b82814fc95356ea743
kaungmyatkyawtiti <eucalyptustiti2003@gmail.com>
2025-10-18 13:47:11 +0630

    fix(neovim): disable noice plugin

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,17 +1,6 @@
 return {
   {
     "folke/noice.nvim",
-    require("noice").setup({
-      routes = {
-        {
-          filter = {
-            event = "lsp",
-            kind = "progress",
-            find = "jdtls",
-          },
-          opts = { skip = true },
-        },
-      },
-    }),
+    enabled = false,
   },
 }
